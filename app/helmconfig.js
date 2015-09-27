8f1ab15840e9e1e0c118b910fd556e352231a5b8
Hsin <shinsonh@gmail.com>
2015-09-27 13:19:52 -0400

    add emailing tool

--- app/helmconfig.js
+++ app/helmconfig.js
@@ -23,7 +23,8 @@ const config = {
     // Add to homescreen for Safari on IOS 
     { "rel": "apple-touch-icon", "sizes": "152x152", applecon },
     { "rel": "stylesheet", "href": "https://fonts.googleapis.com/css?family=Roboto+Condensed", "type": "text/css" },
-    { "rel": "stylesheet", "href": "/assets/styles/main.css" }
+    { "rel": "stylesheet", "href": "/assets/styles/main.css" },
+    { "rel": "stylesheet", "href": "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css", "type": "text/css" }
     // SEO: If your mobile URL is different from the desktop URL, 
     // add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones
     // { "rel": "canonical", "href": "http://www.example.com/" }
---
+++ server/config/secrets.js
@@ -0,0 +1,10 @@
+/** Important **/
+/** You should not be committing this file to GitHub **/
+/** Repeat: DO! NOT! COMMIT! THIS! FILE! TO! YOUR! REPO! **/
+
+module.exports = {
+  // Find the appropriate database to connect to, default to localhost if not found.
+  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/ReactWebpackNode',
+  sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
+  mandrill: process.env.MANDRILL_APIKEY || 'VoJacDE-tM2ZLYw26VCqSg'
+};
